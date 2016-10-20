edfed971f0ce73668c3300388d00d506b7a1121b
MikeeAgpawan <mikaela.agpawan418@west-mec.org>
2016-10-20 14:40:19 -0700

    added JS

---
+++ TemperatureConverter.js
@@ -0,0 +1,10 @@
+function convert(degree) {
+    var x;
+    if (degree == "celsius") {
+        x = document.getElementById("celsius").value * 9 / 5 + 32;
+        document.getElementById("fahrenheit").value = Math.round(x);
+    } else {
+        x = (document.getElementById("fahrenheit").value -32) * 5 / 9;
+        document.getElementById("celsius").value = Math.round(x);
+    }
+}
